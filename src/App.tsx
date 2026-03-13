275c6ff6707864208766e5e4222381098ab137d2
bortnikdiana77712 <bortnikdiana77712@gmail.com>
2026-03-13 15:10:55 +0300

    Add routing and basic pages (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { MainLayout } from "./components";
+import { Route, Routes } from "react-router";
+import {
+  Main,
+  Catalog,
+  Library,
+  Collections,
+  MoviePage,
+  Profile,
+} from "./pages/index.ts";
+
 function App() {
   return (
-    <>
-      <p>App</p>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Main />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="library" element={<Library />} />
+        <Route path="collections" element={<Collections />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="movie/:id" element={<MoviePage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { Main } from "./main/Main.tsx";
+export { Catalog } from "./catalog/Catalog.tsx";
+export { Library } from "./library/Library.tsx";
+export { Collections } from "./collections/Collections.tsx";
+export { MoviePage } from "./movie/Movie.tsx";
+export { Profile } from "./profile/Profile.tsx";
